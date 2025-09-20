ae60bcfa7f9a3fc161ff5be0dfbee58f95fdc824
HawerPL <hubersob217@gmail.com>
2025-09-20 16:49:19 +0200

    Fixed formatting.

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ init_logging()
 
 if settings.ENABLE_PROFILING:
     import pyroscope
+
     logging.info(f"Enabling Profiling")
     pyroscope.configure(
         application_name=settings.APP_NAME,
@@ -20,7 +21,7 @@ if settings.ENABLE_PROFILING:
         sample_rate=100,
         detect_subprocesses=False,
         oncpu=True,
-        gil_only=True
+        gil_only=True,
     )
 app = FastAPI(title=settings.APP_NAME, version="1.0", log_config=None)
 Instrumentator().instrument(app).expose(app)
@@ -39,12 +40,14 @@ def startup_event():
         or settings.ENABLE_ERROR_MODE_HARD_CRASH
         or settings.ENABLE_ERROR_MODE_SIGKILL
     ):
-        logging.debug(f"""
+        logging.debug(
+            f"""
             MODE_LOG: {settings.ENABLE_ERROR_MODE_LOG}
             MODE_EXCEPT: {settings.ENABLE_ERROR_MODE_EXCEPTION}
             MODE_SOFT: {settings.ENABLE_ERROR_MODE_SOFT_CRASH}
             MODE_HARD: {settings.ENABLE_ERROR_MODE_HARD_CRASH}
-            MODE_SIGKILL: {settings.ENABLE_ERROR_MODE_SIGKILL}""")
+            MODE_SIGKILL: {settings.ENABLE_ERROR_MODE_SIGKILL}"""
+        )
         simulate_failure()
 
 
